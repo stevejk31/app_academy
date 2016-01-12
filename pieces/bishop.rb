ec2a66ad62826a1196d6b41f0dac99d6c3e8fee1
stevejk31 <stevekim31@gmail.com>
2016-01-12 10:44:11 -0800

    bishop validates moves against other pieces

--- pieces/bishop.rb
+++ pieces/bishop.rb
@@ -8,7 +8,7 @@ class Bishop < Piece
     @value = " ♝ "
   end
 
-  def valid_moves
+  def moves
     row, col = @position
     n = @board.grid.length - 1
 
@@ -18,5 +18,36 @@ class Bishop < Piece
 
   end
 
+  def valid_moves
+    filtered_moves = moves
+    curr_row, curr_col = @position
+    moves.each do |pos|
+      potential_row, potential_col = pos
+      obj = @board.grid[potential_row][potential_col]
+
+      offset = (obj.color == @color) ? 0 : 1
+
+      if !obj.color.nil?
+        if curr_row > potential_row && curr_col > potential_col #SW
+          filtered_moves.reject! do |pos|
+            pos[0] >= potential_row - offset && pos[1] <= potential_col - offset
+          end
+        elsif curr_row > potential_row && curr_col < potential_col #NW
+          filtered_moves.reject! do |pos|
+            pos[0] <= potential_row - offset && pos[1] <= potential_col - offset
+          end
+        elsif curr_col < potential_col && curr_row > potential_row #SE
+          filtered_moves.reject! do |pos|
+            pos[0] >= potential_row - offset && pos[1] >= potential_col - offset
+          end
+        elsif curr_col < potential_col && curr_row < potential_row #NE
+          filtered_moves.reject! do |pos|
+            pos[0] <= potential_row - offset && pos[1] >= potential_col - offset
+          end
+        end
+      end
+    end
+    filtered_moves
+  end
 
 end
