8eb2ae50099aa31269be1653ff03d00ab4463149
stevejk31 <stevekim31@gmail.com>
2016-01-12 10:50:07 -0800

    bishop validates moves against other pieces

--- pieces/sliding_moves.rb
+++ pieces/sliding_moves.rb
@@ -21,4 +21,28 @@ module SlidingMoves
     possible_moves
   end
 
+  def filter_lateral(moves, original_obj)
+    filtered_moves = moves
+    curr_row, curr_col = original_obj.position
+    moves.each do |pos|
+      potential_row, potential_col = pos
+      potential_obj = original_obj.board.grid[potential_row][potential_col]
+      offset = (potential_obj.color == original_obj.color) ? 0 : 1
+
+      if !potential_obj.color.nil?
+        if curr_row == potential_row && curr_col > potential_col
+          filtered_moves.reject! { |pos| pos[1] <= potential_col - offset }
+        elsif curr_row == potential_row && curr_col < potential_col
+          filtered_moves.reject! { |pos| pos[1] >= potential_col + offset }
+        elsif curr_col == potential_col && curr_row > potential_row
+          filtered_moves.reject! { |pos| pos[0] <= potential_row - offset }
+        elsif curr_col == potential_col && curr_row < potential_row
+          filtered_moves.reject! { |pos| pos[0] >= potential_row + offset }
+        end
+      end
+
+    end
+    filtered_moves
+  end
+
 end
