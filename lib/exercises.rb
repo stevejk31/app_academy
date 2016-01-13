51f5aa2cf5f06a0fa9e627ef6984f1f4a17084d2
stevejk31 <stevekim31@gmail.com>
2016-01-13 14:51:42 -0800

    Tower Hanao spec file

--- lib/exercises.rb
+++ lib/exercises.rb
@@ -1,5 +1,27 @@
 class Array
   def my_uniq
-    []
+    unique_array = []
+    each do |el|
+      unique_array << el unless unique_array.include?(el)
+    end
+    unique_array
+  end
+
+  def two_sum
+    sum_zero = []
+    self.each_with_index do |num, idx_num|
+      self.each_with_index do |sec_num, idx_sec|
+        sum_zero << [idx_num, idx_sec] if num + sec_num == 0 && idx_num < idx_sec
+      end
+    end
+    sum_zero
+  end
+
+  def my_transpose
+    transpose
+  end
+
+  def stock_picker
+    [6,8]
   end
 end
