28d0c31c6290b26582a951e1a149d6d019b020be
stevejk31 <stevekim31@gmail.com>
2016-02-05 17:26:23 -0800

    Play TicTacToe

--- ticTac.js
+++ ticTac.js
@@ -9,22 +9,37 @@ var reader = readLine.createInterface({
 function ticTacToe() {
   this.grid = [[null,null,null],[null,null,null],[null,null,null]];
 }
-ticTacToe.prototype.promptMove = function(callback){
-  console.log(this.grid);
-  var hg = this;
-  if (this.winCondition()) {
+
+ticTacToe.prototype.promptMove = function(sym, callback){
+  console.log(this.grid[0]);
+  console.log(this.grid[1]);
+  console.log(this.grid[2]);
+  var ttt = this;
+  if (this.winCondition(ttt.otherPlayer(sym)) || this.winCondition(sym)) {
     callback();
   } else {
+    console.log("Current mark: " + sym);
     reader.question("Which row do you want put your mark?(0, 1, 2) : ", function(rowIdx) {
       var userRowIdx = parseInt(rowIdx);
       reader.question("Which column do you want put your mark?(0, 1, 2) : ", function(colIdx) {
         var userColIdx = parseInt(colIdx);
-        if (hg.isValidMove(userRowIdx, userColIdx)) {
-          // mark the board
+        if (ttt.isValidMove(userRowIdx, userColIdx)) {
+          ttt.grid[userRowIdx][userColIdx] = sym;
+          sym = ttt.otherPlayer(sym);
+          ttt.promptMove(sym, callback);
+        } else {
+          ttt.promptMove(sym, callback);
         }
-        hg.promptMove(callback);
       });
     });
+  }
+};
+
+ticTacToe.prototype.otherPlayer = function(sym) {
+  if (sym === this.p1m) {
+    return this.p2m;
+  } else {
+    return this.p1m;
   }
 };
 
@@ -39,12 +54,60 @@ ticTacToe.prototype.isValidMove = function(rowIdx, colIdx) {
   return false;
 };
 
-ticTacToe.prototype.winCondition = function(){
-  
+ticTacToe.prototype.winCondition = function(sym){
+  var idx;
+  var idx2;
+  var vert;
+  // horizontal case
+  for (idx = 0; idx < 3; idx++){
+    if (this.grid[idx] === [sym,sym,sym]){
+      return true;
+    }
+  }
+  // vertical case
+  for (idx = 0; idx < 3; idx++){
+    vert = 0;
+    for (idx2 = 0; idx2 < 3; idx2++){
+      if (this.grid[idx2][idx] === sym){
+        vert ++;
+        if (vert === 3) {
+          return true;
+        }
+      }
+    }
+  }
+  //diagonal 1
+  vert = 0;
+  for (idx = 0; idx < 3; idx++){
+    if (this.grid[idx][idx] === sym){
+      vert ++;
+      if (vert === 3) {
+        return true;
+      }
+    }
+  }
+  //diagonal 2
+  vert = 0;
+  for (idx = 0; idx < 3; idx++){
+    if (this.grid[2-idx][idx] === sym){
+      vert ++;
+      if (vert === 3) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 ticTacToe.prototype.run = function(){
-  this.promptMove(completionCallback);
+  var ttt = this;
+  reader.question("Player one mark: ", function(p1m) {
+    ttt.p1m = p1m;
+    reader.question("Player two mark: ", function(p2m) {
+      ttt.p2m = p2m;
+      ttt.promptMove(ttt.p1m, completionCallback);
+    });
+  });
 };
 
 var completionCallback = function() {
@@ -53,5 +116,5 @@ var completionCallback = function() {
 };
 
 
-var hg = new ticTacToe();
-hg.run();
+var potato = new ticTacToe();
+potato.run();
