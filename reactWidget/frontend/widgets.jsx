7cd890b5cda104ad5f78fb06e5bcf4c74037bedd
stevejk31 <stevekim31@gmail.com>
2016-02-15 15:33:45 -0800

    Add autocomplete widget

--- reactWidget/frontend/widgets.jsx
+++ reactWidget/frontend/widgets.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var Tabs = require('./tabs.jsx');
 var Clock = require('./clock.jsx');
 var Weather = require('./weather.jsx');
+var Autocomplete = require('./autocomplete.jsx');
 
 var tabObjects = [
   {title: "Hello", content: "It's Me"},
@@ -10,6 +11,17 @@ var tabObjects = [
   {title: "Title3", content: "content3"},
   {title: "Title4", content: "content4"},
   {title: "Title5", content: "content5"}
+];
+
+var autoCompleteNames = [
+  "aab",
+  "abc",
+  "bba",
+  "bcd",
+  "cde",
+  "cgh",
+  "rafael",
+  "steve"
 ];
 
 var Widgets = React.createClass({
@@ -20,6 +32,7 @@ var Widgets = React.createClass({
           <Tabs tabObjects={tabObjects} />
           <Clock />
           <Weather />
+          <Autocomplete names={autoCompleteNames} />
       </div>
     );
   }
