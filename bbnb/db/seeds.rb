fc008ec620804c88a0d60b8d6b36f5367576fdfa
stevejk31 <stevekim31@gmail.com>
2016-02-19 15:41:39 -0800

    go BAck to backend Build Store

--- bbnb/db/seeds.rb
+++ bbnb/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Bench.create!(lat: 37.775785, lon: -122.445979, description: "NOOOPPPPAAAAA bench" )
+Bench.create!(lat: 37.772045, lon: -122.437015, description: "Castro's Best Bench" )
+Bench.create!(lat: 37.781899, lon: -122.410426, description: "Prime Market Bench" )
